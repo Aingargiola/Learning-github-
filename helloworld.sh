86633b81cb6ec607ef5754026b675f2062d4b5ac
aingargiola <aingargiola8@gmail.com>
2021-12-15 07:59:24 -0600

    initial commit

---
+++ helloworld.sh
@@ -0,0 +1,31 @@
+ifconfig
+eno1: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
+        ether 90:b1:1c:8e:96:43  txqueuelen 1000  (Ethernet)
+        RX packets 0  bytes 0 (0.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 0  bytes 0 (0.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+        device interrupt 20  memory 0xf7c00000-f7c20000  
+
+lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
+        inet 127.0.0.1  netmask 255.0.0.0
+        inet6 ::1  prefixlen 128  scopeid 0x10<host>
+        loop  txqueuelen 1000  (Local Loopback)
+        RX packets 32093  bytes 3636059 (3.6 MB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 32093  bytes 3636059 (3.6 MB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+wlx984827e725cb: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+        inet 192.168.1.61  netmask 255.255.255.0  broadcast 192.168.1.255
+        inet6 fe80::f8d:a514:c495:5a78  prefixlen 64  scopeid 0x20<link>
+        inet6 2600:6c40:467f:c9d7::128a  prefixlen 128  scopeid 0x0<global>
+        inet6 2600:6c40:467f:c9d7:5425:8629:a875:8338  prefixlen 64  scopeid 0x0<global>
+        inet6 2600:6c40:467f:c9d7:b47e:4898:a2af:c051  prefixlen 64  scopeid 0x0<global>
+        ether 98:48:27:e7:25:cb  txqueuelen 1000  (Ethernet)
+        RX packets 12143  bytes 8784962020 (8.7 GB)
+        RX errors 0  dropped 19480  overruns 0  frame 0
+        TX packets 3291  bytes 426579883 (426.5 MB)
+        TX errors 0  dropped 3 overruns 0  carrier 0  collisions 0
+
+
