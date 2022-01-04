9b8fbcea2f21d87f5f1d5636d29d1f24f5e248c0
Anthony Ingargiola <AIngargiola@Heathers-MacBook-Pro.local>
2022-01-03 20:51:31 -0600

    learning

--- specs.sh
+++ specs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sysctl -n machdep.cpu.brand_string
+echo CPU Specs
 system_profiler SPHardwareDataType
+echo Memory
 system_profiler SPDisplaysDataType
+echo Display Setting
 networksetup -listallhardwareports
-
+echo Network Hardware
 
