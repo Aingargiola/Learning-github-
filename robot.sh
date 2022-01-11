555131134852f81425a4940892e4144ca16f9866
aingargiola <aingargiola@gmail.com>
2022-01-10 18:38:16 -0600

    triangle

---
+++ robot.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# echo -n "Enter the name of a country: "
+# read COUNTRY
+
+# echo -n "The official language of $COUNTRY is "
+
+# case $COUNTRY in
+
+#   Lithuania)
+#     echo -n "Lithuanian"
+#     ;;
+
+#   Romania | Moldova)
+#     echo -n "Romanian"
+#     ;;
+
+#   Italy | "San Marino" | Switzerland | "Vatican City")
+#     echo -n "Italian"
+#     ;;
+
+#   *)
+#     echo -n "unknown"
+#     ;;
+# esac
+
+echo -n "how is your day "
+read day
+
+case $day in
+good) 
+    echo -n "i dont care $user"
+    ;;
+
+bad)    
+    echo -n "thats good"
+    ;;
+    esac
+
+# echo -n "how is your day "
+# read day
+
+# case $day in
+# good)
+# if $good
+#     then
+#     echo -n "i dont care $user"
+# else 
+#     echo -n "sucks for you"
+# fi
+# ;;
+#     esac
+
+
+
+   
