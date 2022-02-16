cf26c1e477d6efb6a8fb9149725d3522cbd730d2
aingargiola <aingargiola@gmail.com>
2022-02-15 21:08:39 -0600

    updated

--- python/vehicle.py
+++ python/vehicle.py
@@ -156,3 +156,41 @@ class Car(Vehicle):
 
 Cars= Car("audi", 240, 18)
 print ("The car color is", Cars.color,"The Cars name is", Cars.name, "The max speed is", Cars.max_speed,"the mileage is", Cars.mileage)
+
+
+#Exercise 5
+
+# Create a Bus child class that inherits from the Vehicle class. The default fare charge of any vehicle is 
+# seating capacity * 100. If Vehicle is Bus instance, we need to add an extra 10% on full fare as a maintenance 
+# charge. So total fare for bus instance will become the final amount = total fare + 10% of the total fare.
+
+# Note: The bus seating capacity is 50. so the final fare amount should be 5500. You need to override the fare() 
+# method of a Vehicle class in Bus class.
+
+# Use the following code for your parent Vehicle class. We need to access the parent class from inside a method of 
+# a child class.
+
+#Expected Output:
+
+#Total Bus fare is: 5500.0
+
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100 
+       
+        
+
+class Bus(Vehicle):
+    def fare(self):
+        x = super().fare() 
+        x += x * 10 / 100
+        return x
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
