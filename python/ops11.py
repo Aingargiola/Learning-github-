d2ba2c331436adca6770d688c8333e6fdb26e9c6
aingargiola <aingargiola@gmail.com>
2022-03-09 18:38:41 -0600

    ops11 python

--- python/ops11.py
+++ python/ops11.py
@@ -2,8 +2,9 @@
 # Using file handling commands, create a Python script that creates a new .txt file,
 #  appends three lines, prints to the screen the first line, then deletes the .txt file.
 import os
+
 f = open('ops.txt', 'w' )
-f.write("this file has words")
-f.write("The dog ran accross the street")
-f.write("This is the thrid line")
-os.remove('ops.txt')
+f.write("this file has words\n the dog ran\n very fast")
+b = f.read
+print(b)
+# os.remove('ops.txt')
