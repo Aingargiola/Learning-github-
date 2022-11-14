f9f8130b30bcfbe4aab213154a4c0caba55f84b2
Anthony Ingargiola <AIngargiola@Heathers-MBP.lan>
2022-11-14 17:27:00 -0600

    teaching new students

--- teach.sh
+++ teach.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 echo "Lets do math"
-
-Number1=9
-
+#This is a variable we call a variable by using $ + variable name
+ #Number1=9
+echo "Please enter a number"
+read Number1
 Number2=11
-echo "Your total is "
-total=$((Number1 * Number2))
 
+# We can do math inside a variable by coding like the example below
+total=$((Number1 * Number2))
+#we can use a variable and user created text when echo stuff out also
 echo $total
+echo "your total is "  $total
 
 # echo $Number1
 # echo $Number2
 
+#This is an array were we can put multiple items in a variable and call or only certain ones
+names=("andrew" "anthony" "ashley")
 
+echo ${names[@]}
+echo ${names[2]}
+echo ${names[@]: 1:2}
 
-
+#This is an if and else statment
+if (( $Number1 > 5 ))
+    then echo "Number is greater than 5"
+elif (( $Number1 < 4 ))
+then    
+     echo "Number is less than 5"
+ else
+     echo "Number is 5"
+  fi
